b46bbc4afc6334424513e16fe88a5934734ff083
Kim-Jae-He <jgim78628@gmail.com>
2023-10-26 02:59:50 +0900

    11-16

--- server/app.js
+++ server/app.js
@@ -76,7 +76,7 @@ app.get('/api/employees', (req, res) => {
 // 사원 한명 추가
 app.post('/api/employees', () => {});
 
-
+app.post('/api/employees', () => {});
 
 // 토큰을 전달받아서 로그인한 사람의 email 주소를 되돌려주는 api
 app.get('/api/loggedInId', (req, res) => {
