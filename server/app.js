b248d9385783fa03505d3c80712d9e351230a2dc
Kim-Jae-He <jgim78628@gmail.com>
2023-11-17 02:45:09 +0900

    11-17

--- server/app.js
+++ server/app.js
@@ -8,16 +8,22 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 // require( 'dotenv').config({path:'../.env.local'});
+
 const dotenv = require('dotenv');
 dotenv.config({ path: '../.env.project' });
 
+
+
+const app = express();
+const port = 3002;
+
 // mysql 설정
 
-console.log(process.env.DB_HOST);
-console.log(process.env.DB_USER);
-console.log(process.env.DB_DATABASE);
-console.log(process.env.DB_PASSWORD);
-console.log(process.env.DB_PORT);
+// console.log(process.env.DB_HOST);
+// console.log(process.env.DB_USER);
+// console.log(process.env.DB_DATABASE);
+// console.log(process.env.DB_PASSWORD);
+// console.log(process.env.DB_PORT);
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
@@ -27,20 +33,73 @@ const pool = mysql.createPool({
   port: process.env.DB_PORT,
 });
 
-// import express from "express";
-// const cors = require('cors');
-
-const app = express();
-const port = 3002;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.get('/api', (req, res) => {
   res.send('김제희!');
 });
 
-app.get('/api/users/:userId', async(req, res ) => {
+const loginList = [
+  {
+    id: 'aaa',
+    password: '1234',
+    phone: '01012345678',
+  },
+];
+
+app.get('/api/loginup', (req, res) => {
+  res.json(loginList);
+});
+
+app.get('/api/loginup', (req, res) => {
+  const { id, password, phone } = req.body;
+  loginList.push({
+    id,
+    password,
+    phone,
+  });
+  return res.send('성공입니다');
+});
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+// 모든 사원 조회
+app.get('/api/employees', (req, res) => {
+  // mysql에서 employees 테이블 모든 행,컬럼 조회
+  pool.query('SELECT * FROM employees', (err, rows, fields) => {
+    console.log('ddd');
+    console.log(err);
+    res.json(rows);
+  });
+});
+
+// 사원 한명 추가
+app.post('/api/employees', () => {});
+
+// 토큰을 전달받아서 로그인한 사람의 email 주소를 되돌려주는 api
+app.get('/api/loggedInId', (req, res) => {
+  // 리액트로부터 전달받은 토큰이 정상적인지 확인하고
+  // 정상적이지 않으면 오류로 응답
+  // 정상적이면 email주소로 응답
+  // 토큰은 요청 header 의 Authorization에 Bearer .....
+  // console.log(req.headers.authorization)
+  // 문자열
+  const token = req.headers.authorization.replace('Bearer ', '');
+  // console.log(token);
+  // token은 로그인 당시 발급 받은 토큰
+
+  try {
+    let result = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(result);
+
+    res.send(result.email);
+  } catch (err) {
+    console.log(err);
+    res.status(403).json('오류발생!');
+  }
+});
+
+app.get('/api/users/:userId', async (req, res) => {
   const userId = req.prams.userId;
 
   try {
--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import HeaderPages from './components/header';
 import IndexPage from './pages';
 import LoginPage from './pages/login';
 import JoinPage from './pages/join';
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
+
 
 const router = createBrowserRouter([
   { path: '/main', element: <HeaderPages /> },
@@ -14,8 +15,15 @@ const router = createBrowserRouter([
 
 export const UserContext = createContext();
 
-function App() {
-  return <RouterProvider router={router} />;
-}
+const App = () => {
+  // 전역에서 사용할 변수
+  const [accessToken, setAccessToken] = useState(null);
+  return (
+    <UserContext.Provider value={{ accessToken, setAccessToken }}>
+      <RouterProvider router={router} />
+    </UserContext.Provider>
+  );
+};
 
 export default App;
+ 
