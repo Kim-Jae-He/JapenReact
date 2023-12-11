b55a736cf56e2a296986225e74ac7201fceed5b1
Kim-Jae-He <jgim78628@gmail.com>
2023-12-11 23:07:07 +0900

    12-11

--- src/components/header.js
+++ src/components/header.js
@@ -95,7 +95,7 @@ const HeaderPages = (props) => {
               <HeaderSmallContainer>
                 <LoginContainer
                   onMouseOver={(e) => {
-                    LoginJoinCon(e, 'rgb(222, 226, 230)');
+                    LoginJoinCon(e, 'rgb(222, 226, 231)');
                   }}
                   onMouseOut={(e) => {
                     LoginJoinCon(e, 'white');
